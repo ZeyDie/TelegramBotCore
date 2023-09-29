be73b3b17012714df094c1151dbc18b86133b7cb
Your Name <you@example.com>
2023-09-29 19:48:38 +0500

    J17 record use

--- src/main/java/com/zeydie/telegrambot/api/configs/AbstractFileConfig.java
+++ src/main/java/com/zeydie/telegrambot/api/configs/AbstractFileConfig.java
@@ -8,18 +8,11 @@ import org.jetbrains.annotations.NotNull;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@Data
-@RequiredArgsConstructor
-public final class AbstractFileConfig {
+public record AbstractFileConfig(@NotNull Class<?> parent, @NotNull String name) {
     public static final Path CONFIG_SERVER_FOLDER = Paths.get("configs", "server");
     public static final Path CACHE_FOLDER = Paths.get("cache");
 
-    @NotNull
-    private final Class<?> parent;
-    @NotNull
-    private final String name;
-
     public @NotNull <T> T init() {
-        return (T) new SGsonFile(CONFIG_SERVER_FOLDER.resolve(this.getName())).fromJsonToObject(this.parent);
+        return (T) new SGsonFile(CONFIG_SERVER_FOLDER.resolve(this.name)).fromJsonToObject(this.parent);
     }
 }

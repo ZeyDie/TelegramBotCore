91c904860f0711a6fef0877efc5548fb696c29f9
Zalov-DV <zeydie.dev@gmail.com>
2023-10-12 16:04:45 +0500

    refactoring

--- src/main/java/com/zeydie/telegrambot/api/configs/data/BotFileConfig.java
+++ src/main/java/com/zeydie/telegrambot/api/configs/data/BotFileConfig.java
@@ -1,10 +1,12 @@
 package com.zeydie.telegrambot.api.configs.data;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.NonFinal;
 import org.jetbrains.annotations.NotNull;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public final class BotFileConfig {
     @NonFinal
     private @NotNull String name = "name";

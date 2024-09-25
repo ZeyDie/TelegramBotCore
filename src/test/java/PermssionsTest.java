b3daa8475e3c6d3434fe754d1a0cae4f911e1dfd
ZeyDi <zeydi.dev@gmail.com>
2024-09-25 11:47:26 +0500

    fix tests

--- src/test/java/PermssionsTest.java
+++ src/test/java/PermssionsTest.java
@@ -1,8 +1,5 @@
-import com.zeydie.telegrambot.TelegramBotCore;
 import com.zeydie.telegrambot.api.modules.permissions.IPermissions;
-import com.zeydie.telegrambot.api.modules.permissions.data.PermissionData;
-import lombok.extern.java.Log;
-import lombok.val;
+import com.zeydie.telegrambot.modules.permissions.impl.UserPermissionsImpl;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.*;
 
@@ -17,18 +14,22 @@ public class PermssionsTest {
 
     @BeforeAll
     public static void init() {
-        permissions = TelegramBotCore.getInstance().getPermissions();
+        permissions = new UserPermissionsImpl();
+
+        permissions.preInit();
+        permissions.init();
+        permissions.postInit();
     }
 
     @Test
     @Order(1)
-    public static void userIsNotExist() {
+    public void userIsNotExist() {
         Assertions.assertNull(permissions.getPermissionData(userId));
     }
 
     @Test
     @Order(2)
-    public static void addPermission() {
+    public void addPermission() {
         permissions.addPermission(userId, permissionName);
 
         Assertions.assertTrue(permissions.hasPermission(userId, permissionName));
@@ -36,13 +37,13 @@ public class PermssionsTest {
 
     @Test
     @Order(3)
-    public static void userIsExist() {
+    public  void userIsExist() {
         Assertions.assertNotNull(permissions.getPermissionData(userId));
     }
 
     @Test
     @Order(4)
-    public static void removePermission() {
+    public void removePermission() {
         permissions.removePermission(userId, permissionName);
 
         Assertions.assertFalse(permissions.hasPermission(userId, permissionName));
@@ -50,7 +51,7 @@ public class PermssionsTest {
 
     @Test
     @Order(5)
-    public static void addPermissions() {
+    public  void addPermissions() {
         permissions.addPermission(userId, permissionName + 1);
         permissions.addPermission(userId, permissionName + 2);
         permissions.addPermission(userId, permissionName + 3);
@@ -62,7 +63,7 @@ public class PermssionsTest {
 
     @Test
     @Order(6)
-    public static void removePermissions() {
+    public void removePermissions() {
         permissions.removePermission(userId, permissionName + 1);
         permissions.removePermission(userId, permissionName + 2);
         permissions.removePermission(userId, permissionName + 3);
@@ -74,14 +75,14 @@ public class PermssionsTest {
 
     @Test
     @Order(7)
-    public static void addPermissionsRandom() {
+    public void addPermissionsRandom() {
         for (int i = 0; i < new Random().nextInt(5); i++)
             permissions.addPermission(userId, RandomStringUtils.randomAlphabetic(10));
     }
 
     @Test
     @Order(8)
-    public static void removePermissionsRandom() {
+    public void removePermissionsRandom() {
         permissions.removePermissions(userId);
 
         Assertions.assertNull(permissions.getPermissionData(userId));
